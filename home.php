963f0bde3e103d0b38e1a9987734b340932352f7
myathinkyu <myathinkyu.pp@gmail.com>
2023-09-26 17:33:19 +0630

    89-105

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+
+class home{
+    public function __construct()
+    {
+        echo "I am constructor of " . __CLASS__ . " class<br>";
+    }
+    public function index()
+    {
+        echo "I am index method of " . __CLASS__ . " class<br>";
+    }
+}
+
+
+
+
+
+
+?>
